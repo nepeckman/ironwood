b86cb2d6d911dca4077b8d56234f73151bf499ba
nepeckman <elijahpeckman@gmail.com>
2018-11-03 15:05:11 -0400

    Refactored switch action accessor

--- src/engine/actionAccessor.nim
+++ src/engine/actionAccessor.nim
@@ -5,16 +5,20 @@ import
   gameObjects/gameObjects,
   state, action, pokemonAccessor
 
-func moveValidator(pokemon: Pokemon, move: PokeMove, state: State): bool =
+func moveValidator(state: State, pokemon: Pokemon, move: PokeMove): bool =
   let allyTeam = state.getTeam(pokemon)
-  `not`((gckTaunted in pokemon.conditions or pokemon.item == "Assualt Vest") and move.category == pmcStatus) and
+  return `not`((gckTaunted in pokemon.conditions or pokemon.item == "Assualt Vest") and move.category == pmcStatus) and
     `not`(move.isZ and allyTeam.isZUsed)
   # TODO: Choice lock, tormet lock. Both can be done by implementing last move used. Also would help mimic.
   # Disabled lock
   # Check move failure, don't provide moves that will always fail
 
+func switchValidator(state: State, actingPokemon, teammate: Pokemon): bool =
+  return not state.isActive(teammate) and not teammate.fainted and
+    state.isActive(actingPokemon) and not actingPokemon.fainted
+
 func possibleMoves*(state: State, pokemon: Pokemon): seq[PokeMove] =
-  pokemon.moves.filter((move) => moveValidator(pokemon, move, state))
+  pokemon.moves.filter((move) => state.moveValidator(pokemon, move))
 
 func possibleTargets*(state: State, move: PokeMove): seq[set[AttackTargetKind]] =
   case move.target
@@ -34,9 +38,6 @@ func possibleActions*(state: State, pokemonID: UUID): seq[Action] =
   for move in state.possibleMoves(pokemon):
     for targets in possibleTargets(state, move):
       result.add(newMoveAction(pokemon.uuid, move, targets))
-  for teammate in team:
-    if not state.isActive(teammate) and not teammate.fainted:
-      result.add(newSwitchAction(pokemon.uuid, teammate.uuid))
 
 func possibleActions*(state: State, pokemonIDs: seq[UUID]): seq[Action] =
   result = @[]
@@ -59,17 +60,14 @@ func getMoveAction(actions: seq[Action], move: string): Action =
 func getMoveAction*(state: State, pokemonID: UUID, move: string): Action =
   getMoveAction(state.possibleActions(pokemonID), move)
 
-func getSwitchAction(state: State, actions: seq[Action], switchTargetID: UUID): Action =
-  for action in actions:
-    if action.kind == akSwitchSelection and 
-       action.switchTargetID == switchTargetID:
-      return action
-  var error = new(CatchableError)
-  error.msg = "No action for switch: " & $switchTargetID
-  raise error
-
 func getSwitchAction*(state: State, pokemonID: UUID, switchTargetID: UUID): Action =
-  getSwitchAction(state, state.possibleActions(pokemonID), switchTargetID)
+  let actingPokemon = state.getPokemon(pokemonID)
+  let teammate = state.getPokemon(switchTargetID)
+  if state.switchValidator(actingPokemon, teammate):
+    return newSwitchAction(pokemonID, switchTargetID)
+  var error = new(CatchableError)
+  error.msg = "No action for switch: " & teammate.name
+  raise error
 
 func getMegaEvolutionAction(state: State, pokemonID: UUID): Action =
   let pokemon = state.getPokemon(pokemonID)
